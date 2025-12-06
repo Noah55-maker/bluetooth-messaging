3d302689ca6fdf8cb0adc02eae3511f1a5496f32
Noah <74223879+Noah55-maker@users.noreply.github.com>
2025-12-06 15:08:02 -0800

    Add decryptString and encryptString functions
    These two takes string arguments to simplify function calls

--- crypto/crypto.py
+++ crypto/crypto.py
@@ -1,33 +1,37 @@
 import random
+import base64
 from Crypto.Cipher import AES
 
 def encrypt(message, key):
-	cipher = AES.new(key, AES.MODE_GCM)
+    cipher = AES.new(key, AES.MODE_GCM)
 
-	nonce = cipher.nonce
-	ciphertext, tag = cipher.encrypt_and_digest(message)
-	return [nonce, ciphertext, tag]
+    nonce = bytes(cipher.nonce)
+    ciphertext, tag = cipher.encrypt_and_digest(message)
+    return [nonce, ciphertext, tag]
+
+def encryptString(message, key):
+    b = bytearray()
+    b.extend(map(ord, message))
+    return encrypt(b, key)
 
 def decrypt(key, nonce, ciphertext, tag):
-	cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
-	plaintext = cipher.decrypt(ciphertext)
+    cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+    plaintext = cipher.decrypt(ciphertext)
 
-	try:
-		cipher.verify(tag)
-		print("the message is authentic")
-	except ValueError:
-		print("Bad message")
+    try:
+        cipher.verify(tag)
+        print("the message is authentic")
+    except ValueError:
+        print("Bad message")
 
-	return plaintext
+    return plaintext
+
+def decryptString(key, nonce, ciphertext, tag):
+    return decrypt(key, nonce, ciphertext, tag).decode("utf-8")
 
 key = bytearray(16)
 for i in range(16):
-	key[i] = random.randint(0, 255)
+    key[i] = random.randint(0, 255)
 
-st = "This is my secret message"
-b = bytearray()
-b.extend(map(ord, st))
-n, c, t = encrypt(b, key)
-
-pt = decrypt(key, n, c, t)
-print(pt.decode("utf-8"))
+n, c, t = encryptString("Secret message", key)
+print(decryptString(key, n, c, t))
